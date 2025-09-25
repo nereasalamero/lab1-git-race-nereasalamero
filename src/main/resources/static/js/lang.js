68e6732f10eb33066d8f6b9d2f94f92c836e77d6
Nerea Salamero <820632@unizar.es>
2025-09-25 10:12:59 +0200

    New feature: multi-language
    - First steps of internationalisation (i18n)
    - Fix previous bugs (tests)

---
+++ src/main/resources/static/js/lang.js
@@ -0,0 +1,9 @@
+/**
+ * Function that saves the language when the user changes it.
+ */
+document.querySelectorAll(".lang-switch").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const lang =
+    })
+})
